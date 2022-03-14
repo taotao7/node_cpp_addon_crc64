9f8286f0d6e12af898eccd9ab550cd68de096a83
taotao7 <moca_tao7@foxmail.com>
2022-03-14 18:05:23 +0800

    feat: touch cmakelists.txt

---
+++ CMakeFiles/crc64.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/crc64.dir/crc64.cpp.o"
+  "CMakeFiles/crc64.dir/crc64.cpp.o.d"
+  "crc64"
+  "crc64.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/crc64.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ crc64.cpp
@@ -0,0 +1,5 @@
+#include <cstdint>
+#include <iostream>
+#include <node.h>
+
+#define EMCAPOLY UINT64_C(0xc96c5795d7870f42)
